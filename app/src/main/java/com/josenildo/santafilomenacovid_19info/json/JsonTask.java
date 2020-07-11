ffa75fa98c0a6bad7c6ccf76b55cb234abf1dc26
José Nildo <58955100+JoseNildo-Dev@users.noreply.github.com>
2020-07-11 13:13:36 -0300

    Criação do JsonTask.java
    Criação do pacote json, no qual contem a classe JsonTask, responsavel por obter os dados (em Json) da api REST

---
+++ app/src/main/java/com/josenildo/santafilomenacovid_19info/json/JsonTask.java
@@ -0,0 +1,95 @@
+package com.josenildo.santafilomenacovid_19info.json;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class JsonTask extends AsyncTask<String, String, String> {
+
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    protected String doInBackground(String... params) {
+
+        HttpURLConnection connection = null;
+        BufferedReader reader = null;
+
+        try {
+            URL url = new URL(params[0]);
+            connection = (HttpURLConnection) url.openConnection();
+            connection.connect();
+
+            InputStream stream = connection.getInputStream();
+            reader = new BufferedReader(new InputStreamReader(stream));
+            StringBuffer buffer = new StringBuffer();
+
+            String line = "";
+
+            while ((line = reader.readLine()) != null) {
+                buffer.append(line+"\n");
+                Log.d("Response: ", "> " + line);
+            }
+            return buffer.toString();
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
+            try {
+                if (reader != null) {
+                    reader.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        super.onPostExecute(result);
+
+        Log.i("meuLog",""+result);
+
+        /*
+        try {
+            JSONObject listaJson = new JSONObject(result);
+            titulo.setText(listaJson.getString("titulo"));
+            JSONArray questionario = listaJson.getJSONArray("questionario");
+
+            for(int i=0; i<questionario.length(); i++){
+                JSONObject questao = questionario.getJSONObject(i);
+
+                String perg = questao.getString("Pergunta");
+                String ra = questao.getString("respA");
+                String rb = questao.getString("respB");
+                String rc = questao.getString("respC");
+                int correta = questao.getInt("correta");
+
+                Questao minhaQuestao = new Questao(perg,ra,rb,rc,correta);
+                questoes.add(minhaQuestao);
+
+            }
+        }catch (JSONException e){e.printStackTrace();}
+
+
+         */
+    }
+}

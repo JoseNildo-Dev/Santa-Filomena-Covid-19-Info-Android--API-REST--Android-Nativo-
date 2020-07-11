d59efb8f2c0e2292af2f0a3e5668611c57886f33
José Nildo <58955100+JoseNildo-Dev@users.noreply.github.com>
2020-07-11 13:21:29 -0300

    Tentativa de obter os dados JSON da api e mostra-los no LogCat

--- app/src/main/java/com/josenildo/santafilomenacovid_19info/MainActivity.java
+++ app/src/main/java/com/josenildo/santafilomenacovid_19info/MainActivity.java
@@ -4,11 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.josenildo.santafilomenacovid_19info.json.JsonTask;
+
 public class MainActivity extends AppCompatActivity {
+
+    // String contendo o link para a api REST, obtendo os dados de Santa Filomena a partir do código
+    // da cidade fornecido pelo IBGE (2612554).
+    String jsonUrl = "https://brasil.io/api/dataset/covid19/caso/data/?format=json&is_last=True&state=PE&city_ibge_code=2612554";
+    //String jsonUrl = "https://brasil.io/api/dataset/covid19/caso_full/data/?format=json&is_last=True&state=PE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new JsonTask().execute(jsonUrl);
     }
 }

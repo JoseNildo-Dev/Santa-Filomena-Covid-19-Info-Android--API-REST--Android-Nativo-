fcbe0199adbf4f2a499d42b421fe1c12e1c3cf9c
José Nildo <58955100+JoseNildo-Dev@users.noreply.github.com>
2020-07-11 15:50:50 -0300

    Obtenção de JSON feita com sucesso e criação da SplashActivity

---
+++ app/src/main/java/com/josenildo/santafilomenacovid_19info/SplashActivity.java
@@ -0,0 +1,14 @@
+package com.josenildo.santafilomenacovid_19info;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
